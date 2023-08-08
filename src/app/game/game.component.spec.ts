7e6697841b5f7657f316d07be765bd17f3fc7052
Kelsey Roy <kroy@8thlight.com>
2023-08-08 12:46:00 -0700

    Creates answerServiceStub in GameComponent spec

--- src/app/game/game.component.spec.ts
+++ src/app/game/game.component.spec.ts
@@ -1,6 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable, from, of, defer } from 'rxjs';
 
 import { GameComponent } from './game.component';
+import { Answer } from '../models/answer';
+import { AnswerService } from '../services/answer.service';
+
+const mockAnswer = { id: 1, word: 'ADEPT' } as Answer;
+
+const answerServiceStub = {
+  getAnswer() {
+    return of(mockAnswer);
+  }
+};
 
 describe('GameComponent', () => {
   let component: GameComponent;
@@ -8,7 +19,8 @@ describe('GameComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [GameComponent]
+      declarations: [GameComponent],
+      providers: [{ provide: AnswerService, useValue: answerServiceStub }]
     });
     fixture = TestBed.createComponent(GameComponent);
     component = fixture.componentInstance;
@@ -17,5 +29,10 @@ describe('GameComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set "ADEPT" as answer', async () => {
+    const answer = component.answer.word;
+    expect(answer).toEqual(mockAnswer.word);
   });
 });
