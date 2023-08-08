19603825b18a3dc7e4c6b2fa0d6168d09d7f9a70
Kelsey Roy <kroy@8thlight.com>
2023-08-08 10:23:02 -0700

    Creates getAnswers in answer service

--- src/app/services/answer.service.ts
+++ src/app/services/answer.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
+import { Answer } from '../models/answer';
+import { ANSWERS } from '../mock-data/mock-answers';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AnswerService {
 
   constructor() { }
+  getAnswers(): Observable<Answer[]> {
+    const answers = of(ANSWERS);
+    return answers;
+  }
 }
