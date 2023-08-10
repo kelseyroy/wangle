ed60d138ff6d4c68d2a75918d3c0137aaf1c4e98
Kelsey Roy <kroy@8thlight.com>
2023-08-10 10:00:00 -0700

    Adds createDb and genId to InMemoryDataService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
+import { InMemoryDataService } from './services/in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { GameComponent } from './game/game.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Answer } from '../models/answer';
+import { ANSWERS } from '../mock-data/mock-answers';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class InMemoryDataService {
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const answers = ANSWERS;
+    return { answers };
+  }
 
-  constructor() { }
+  genId(answers: Answer[]): number {
+    return answers.length > 0 ? Math.max(...answers.map(answer => answer.id)) + 1 : 11;
+  }
 }
