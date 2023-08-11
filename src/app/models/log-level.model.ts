8c737ecc0a56ee5ff958a232339bf08ec2320fbb
Kelsey Roy <kroy@8thlight.com>
2023-08-11 15:02:18 -0700

    Removes LogLevel and logWith()

--- src/app/models/log-level.model.ts
+++
@@ -1,6 +0,0 @@
-export class LogLevel {
-    None = 0;
-    Info = 1;
-    Warn = 2;
-    Error = 3;
-}
--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,36 +1,17 @@
 import { Injectable } from '@angular/core';
-import { LogLevel } from '../models/log-level.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogService {
-  private readonly logLevel: LogLevel = new LogLevel();
 
   info(msg: string): void {
-    this.logWith(this.logLevel.Info, msg);
+    console.info('%c' + msg, 'color: #6495ED');
   }
   warn(msg: string): void {
-    this.logWith(this.logLevel.Warn, msg);
+    console.warn('%c' + msg, 'color: #FF8C00');
   }
   error(msg: string): void {
-    this.logWith(this.logLevel.Error, msg);
-  }
-
-  private logWith(level: number, msg: string): void {
-    if (level <= this.logLevel.Error) {
-      switch (level) {
-        case this.logLevel.None:
-          return console.log(msg);
-        case this.logLevel.Info:
-          return console.info('%c' + msg, 'color: #6495ED');
-        case this.logLevel.Warn:
-          return console.warn('%c' + msg, 'color: #FF8C00');
-        case this.logLevel.Error:
-          return console.error('%c' + msg, 'color: #DC143C');
-        default:
-          console.debug(msg);
-      }
-    }
+    console.error('%c' + msg, 'color: #DC143C');
   }
 }
