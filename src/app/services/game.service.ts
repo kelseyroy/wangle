9959a3402754a23a13b74cf79f05b4c0b2e4b41c
Kelsey Roy <kroy@8thlight.com>
2023-08-15 18:18:23 -0700

    Adds GameService with addGame and getGames functionality

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+import { Game } from '../models/game';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
+  private readonly gamesUrl = `api/games`;
+  private readonly httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  addGame(game: Game): Observable<Game> {
+    return this.http.post<Game>(this.gamesUrl, game, this.httpOptions).pipe(
+      tap((newGame: Game) => console.log(`added hero w/ id=${newGame.id}`)),
+      catchError(err => {
+        console.error(`addGame failed: ${err.message}`);
+        return of(<Game>{});
+      })
+    );
+  }
+
+  getGames(): Observable<Game> {
+    return this.http.get<Game>(this.gamesUrl).pipe(
+      tap(() => console.log('fetched games')),
+      catchError(err => {
+        console.error(`getGames failed: ${err.message}`);
+        return of(<Game>{});
+      }));
+  }
 }
