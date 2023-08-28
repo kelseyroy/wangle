92ad5e042e182fc8320972fae29b858cd367f82c
Kelsey Roy <kroy@8thlight.com>
2023-08-28 16:43:28 -0700

    Updates KeyComponent to change background colors

--- src/app/key/key.component.ts
+++ src/app/key/key.component.ts
@@ -1,13 +1,41 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Observable, distinctUntilChanged, map } from 'rxjs';
+
+import { GamePlayService } from '../services/game-play.service';
+import { KeyScore } from '../models/key-score';
+
+export enum KeyColor {
+  darkGrey = 'dark-grey',
+  lightGrey = 'light-grey',
+  green = 'green',
+  yellow = 'yellow'
+}
 
 @Component({
   selector: 'app-key',
   templateUrl: './key.component.html',
   styleUrls: ['./key.component.scss']
 })
-export class KeyComponent {
+export class KeyComponent implements OnInit {
+  protected keyColor$!: Observable<KeyColor>;
+
   @Input() public key!: string;
   @Output() keyClick = new EventEmitter<string>();
+
+  constructor(protected readonly gameplayService: GamePlayService) { }
+
+  ngOnInit(): void {
+    this.keyColor$ = this.gameplayService.evaluateKey$(this.key).pipe(
+      distinctUntilChanged(),
+      map(val => {
+        if (val === KeyScore.notInWord) return KeyColor.darkGrey;
+        if (val === KeyScore.inWord) return KeyColor.yellow;
+        if (val === KeyScore.correct) return KeyColor.green;
+
+        return KeyColor.lightGrey;
+      })
+    )
+  }
 
   protected isLargeButton(key: string): boolean {
     return key === 'ENTER' || key === 'DELETE';
